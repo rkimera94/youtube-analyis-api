333fd24d27de266f1099a936c82a4b1becd97348
rkimera <rkimera2013@gmail.com>
2022-04-16 15:45:01 +0300

    sql database connection

--- sql_database.py
+++ sql_database.py
@@ -1,7 +1,12 @@
 #from sqlalchemy.orm import declarative_base
+from curses import echo
 from datetime import datetime
-from sqlalchemy import Column, String, DateTime, Integer
+from email.encoders import encode_noop
+from sqlalchemy import Column, String, DateTime, Integer, create_engine
 from sqlalchemy.ext.declarative import declarative_base
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 
 Base = declarative_base()
@@ -9,6 +14,30 @@ Base = declarative_base()
 '''
 class to store  video ids
 '''
+BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+
+# database connection params
+db_user = os.getenv("DB_USERNAME")
+db_pass = os.getenv("DB_PASSWORD")
+db_host = os.getenv("DB_HOST")
+db_name = os.getenv("DB_DATABASE_NAME")
+
+'''
+config database connection
+    - connection string 
+    - create sqlalchemy engine 
+'''
+
+connection_string = f'postgresql://{db_user}:{db_pass}@{db_host}:5432/{db_name}'
+
+# engine = create_engine(
+#     f'postgresql://{db_user}:{db_pass}@{db_host}:5432/{db_name}')
+
+
+engine = create_engine(connection_string, echo=True)
+
+
+''' Video class'''
 
 
 class Video(Base):
@@ -23,6 +52,6 @@ class Video(Base):
         return f"<video video_id ={self.video_id}"
 
 
-new_video = Video(id=1, video_id='47634759', kind='DEMO')
+new_video = Video(id=1, video_id='352yu493430', kind='DEMO')
 
 print('new_video', new_video)
